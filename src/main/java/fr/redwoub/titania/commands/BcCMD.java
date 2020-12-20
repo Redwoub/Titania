c26a5dd5d45ca87368f0bc2a47f820321e4d86a0
MrCubee <contact@mrcubee.fr>
2020-12-20 18:48:49 +0100

    [update] Java conventions and add CMD prefix to Bc and Invsee class.

--- src/main/java/fr/redwoub/titania/commands/BcCMD.java
+++ src/main/java/fr/redwoub/titania/commands/BcCMD.java
@@ -7,31 +7,28 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public class Bc implements CommandExecutor {
+public class BcCMD implements CommandExecutor {
+
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        Player player;
+        StringBuilder stringBuilder;
 
         if(!(sender instanceof Player)){
             sender.sendMessage(ChatColor.RED + "Seul un joueur peut executer cette command");
             return false;
         }
-
-        Player p = (Player) sender;
-
+        player = (Player) sender;
         if(args.length == 0){
-            p.sendMessage(ChatColor.RED + "La commande est : /bc <message>");
+            player.sendMessage(ChatColor.RED + "La commande est : /bc <message>");
             return false;
         }
-
-        if(args.length >= 1){
-
-            StringBuilder sb = new StringBuilder();
-            for(String str : args){
-                sb.append(str + " ");
-            }
-            Bukkit.broadcastMessage("§b[§4Annonce§b]§6 " + sb.toString());
+        stringBuilder = new StringBuilder();
+        for(String str : args){
+            stringBuilder.append(str);
+            stringBuilder.append(' ');
         }
-
+        Bukkit.broadcastMessage("§b[§4Annonce§b]§6 " + stringBuilder.toString());
         return false;
     }
 }
--- src/main/java/fr/redwoub/titania/commands/InvseeCMD.java
+++ src/main/java/fr/redwoub/titania/commands/InvseeCMD.java
@@ -6,30 +6,28 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public class Invsee implements CommandExecutor {
+public class InvseeCMD implements CommandExecutor {
+
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        Player player;
+        Player target;
 
         if(!(sender instanceof Player)){
             sender.sendMessage("§cSeul un joueur peux executer cette commande");
             return false;
         }
-
-        Player p = (Player) sender;
-
+        player = (Player) sender;
         if(args.length == 0){
-            p.sendMessage("§cLa commande est : /invsee <Pseudo du joueur>");
+            player.sendMessage("§cLa commande est : /invsee <Pseudo du joueur>");
             return false;
         }
-
         if(Bukkit.getPlayer(args[0]) == null){
-            p.sendMessage("§cCe joueur n'est pas connecter ou n'existe pas");
+            player.sendMessage("§cCe joueur n'est pas connecter ou n'existe pas");
             return false;
         }
-
-        Player target = Bukkit.getPlayer(args[0]);
-        p.openInventory(target.getInventory());
-
+        target = Bukkit.getPlayer(args[0]);
+        player.openInventory(target.getInventory());
         return false;
     }
 }
