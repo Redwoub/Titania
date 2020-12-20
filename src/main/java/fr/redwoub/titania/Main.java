bc1d2fec172a15e681aa4fc068298e19be315d54
MrCubee <contact@mrcubee.fr>
2020-12-20 18:49:27 +0100

    [upgrade] Player freeze system.

--- src/main/java/fr/redwoub/titania/Main.java
+++ src/main/java/fr/redwoub/titania/Main.java
@@ -1,47 +1,30 @@
 package fr.redwoub.titania;
 
-import fr.redwoub.titania.commands.Bc;
-import fr.redwoub.titania.commands.Freeze;
-import fr.redwoub.titania.commands.Invsee;
+import fr.redwoub.titania.commands.BcCMD;
+import fr.redwoub.titania.commands.FreezeCMD;
+import fr.redwoub.titania.commands.InvseeCMD;
 import fr.redwoub.titania.manager.PlayerManager;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
-import java.util.ArrayList;
-import java.util.UUID;
 
 public class Main extends JavaPlugin {
 
-    private static Main instance;
-    public static ArrayList<UUID> isFreeze = new ArrayList<>();
+    private void registerListener(){
+        PluginManager pm = Bukkit.getPluginManager();
+        pm.registerEvents(new PlayerManager(), this);
+    }
 
+    private void registerCommand(){
+        getCommand("bc").setExecutor(new BcCMD());
+        getCommand("invsee").setExecutor(new InvseeCMD());
+        getCommand("freeze").setExecutor(new FreezeCMD());
+        getCommand("unfreeze").setExecutor(new FreezeCMD());
+    }
 
     @Override
     public void onEnable(){
-        instance = this;
         registerListener();
         registerCommand();
     }
-
-    public static Main getInstance() {
-        return instance;
-    }
-
-    private void registerListener(){
-
-        PluginManager pm = Bukkit.getPluginManager();
-        pm.registerEvents(new PlayerManager(), this);
-
-    }
-
-    public void registerCommand(){
-
-        getCommand("bc").setExecutor(new Bc());
-        getCommand("invsee").setExecutor(new Invsee());
-        getCommand("freeze").setExecutor(new Freeze());
-        getCommand("unfreeze").setExecutor(new Freeze());
-    }
-
-
-
 }
