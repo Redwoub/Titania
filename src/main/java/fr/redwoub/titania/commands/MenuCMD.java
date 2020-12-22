d69462fa87bc6f6a91ab5867a51147b1078e187b
Redwoub <eloiverinaudcoma@gmail.com>
2020-12-22 16:17:29 +0100

    [update] Implement item in Menu

--- src/main/java/fr/redwoub/titania/commands/MenuCMD.java
+++ src/main/java/fr/redwoub/titania/commands/MenuCMD.java
@@ -1,14 +1,21 @@
 package fr.redwoub.titania.commands;
 
+import fr.redwoub.titania.rank.Accouts;
+import fr.redwoub.titania.utils.ItemBuilder;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
 
-public class MenuCMD implements CommandExecutor {
+public class MenuCMD implements CommandExecutor, Listener {
     private Inventory inv;
 
 
@@ -22,7 +29,36 @@ public class MenuCMD implements CommandExecutor {
         Player player = (Player) sender;
 
         inv = Bukkit.createInventory(null, 54, "§6Menu");
+        inv.setItem(53, new ItemBuilder(Material.BARRIER).setName("§cClose Menu").setLore("§7Left click to close this menu.").toItemStack());
+        inv.setItem(0, new ItemBuilder(Material.IRON_INGOT).setName("§6Add 10$").toItemStack());
+
+        player.openInventory(inv);
         return false;
+    }
+
+    @EventHandler
+    public void onInterract(InventoryClickEvent e){
+        Player player = (Player) e.getWhoClicked();
+        ItemStack current = e.getCurrentItem();
+        Accouts accouts = new Accouts(player);
+
+        switch (current.getType()){
+            case IRON_INGOT:
+                if(current.getItemMeta().getDisplayName().equalsIgnoreCase("§6Add 10$")){
+                    accouts.addCoins(10);
+                    player.sendMessage("§aVotre compte a était créditer de 10$\n" + "§aFaite §e/money §apour votre nouveau solde");
+                    e.setCancelled(true);
+                }
+                break;
+
+            case BARRIER:
+                if(current.getItemMeta().getDisplayName().equalsIgnoreCase("§cClose Menu")){
+                    e.setCancelled(true);
+                    player.closeInventory();
+                }
+                break;
+            default: break;
+        }
     }
 
 }
