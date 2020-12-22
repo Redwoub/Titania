99fbcfdb0e913d6f8c2745e232ccc625e3ef66ca
Redwoub <eloiverinaudcoma@gmail.com>
2020-12-22 16:17:40 +0100

    [null] Test

--- src/main/java/fr/redwoub/titania/manager/PlayerManager.java
+++ src/main/java/fr/redwoub/titania/manager/PlayerManager.java
@@ -3,11 +3,14 @@ package fr.redwoub.titania.manager;
 import fr.redwoub.titania.Main;
 import fr.redwoub.titania.rank.Accouts;
 import fr.redwoub.titania.utils.Freeze;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class PlayerManager implements Listener {
 
@@ -28,5 +31,10 @@ public class PlayerManager implements Listener {
         Accouts accouts = new Accouts(player);
         accouts.setup();
         accouts.addCoins(10);
+        ItemStack it = new ItemStack(Material.WOOD_PICKAXE);
+        ItemMeta itm = it.getItemMeta();
+        itm.spigot().setUnbreakable(true);
+        it.setItemMeta(itm);
+        player.getInventory().addItem(it);
     }
 }
