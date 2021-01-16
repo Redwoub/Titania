de29bd7580f0844aa25e74368166f6665fb1da31
Redwoub <eloiverinaudcoma@gmail.com>
2021-01-16 14:36:37 +0100

    ...

--- src/main/java/fr/redwoub/titania/manager/PlayerJoin.java
+++ src/main/java/fr/redwoub/titania/manager/PlayerJoin.java
@@ -4,18 +4,11 @@ package fr.redwoub.titania.manager;
 import fr.redwoub.titania.rank.Accouts;
 import fr.redwoub.titania.rank.RankUnit;
 import org.bukkit.ChatColor;
-import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
-import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 
-import java.util.List;
 
 
 public class PlayerJoin implements Listener {
@@ -37,39 +30,4 @@ public class PlayerJoin implements Listener {
 
     }
 
-    @EventHandler
-    public void onLeft(PlayerQuitEvent e){
-        Player player = e.getPlayer();
-        Accouts accouts = new Accouts(player);
-
-        if(accouts.getRank().getPower() <= RankUnit.MODERATEUR.getPower()){
-            e.setQuitMessage(accouts.getRank().getPrefix() + player.getName() + " §fvien de quitter le serveur");
-        } else {
-            e.setQuitMessage(ChatColor.RED + "- " + ChatColor.WHITE + player.getName());
-        }
-    }
-
-    @EventHandler
-    public void onClick(InventoryClickEvent event) {
-
-        Inventory inv = event.getInventory();
-        Player player = (Player) event.getWhoClicked();
-        ItemStack current = event.getCurrentItem();
-
-        if (current == null) return;
-
-        if (inv.getName().equalsIgnoreCase("§7Sanctions pour ")) {
-
-        }
-    }
-
-    public ItemStack getItem(Material material, String customName, List<String> lore) {
-
-        ItemStack it = new ItemStack(material, 1);
-        ItemMeta itM = it.getItemMeta();
-        if (customName != null) itM.setDisplayName(customName);
-        if (lore != null) itM.setLore(lore);
-        it.setItemMeta(itM);
-        return it;
-    }
 }
--- src/main/java/fr/redwoub/titania/manager/PlayerManager.java
+++ src/main/java/fr/redwoub/titania/manager/PlayerManager.java
@@ -4,15 +4,19 @@ package fr.redwoub.titania.manager;
 import fr.redwoub.titania.rank.Accouts;
 import fr.redwoub.titania.rank.RankUnit;
 import fr.redwoub.titania.utils.Freeze;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.*;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import java.awt.image.PackedColorModel;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -36,6 +40,33 @@ public class PlayerManager implements Listener {
 
         event.setFormat(rankUnit.getPrefix() + "%1$s §7> " + (rankUnit == RankUnit.JOUEUR ? "§7" : "§f") + "%2$s");
     }
+
+    @EventHandler
+    public void onLeft(PlayerQuitEvent e){
+        Player player = e.getPlayer();
+        Accouts accouts = new Accouts(player);
+
+        if(accouts.getRank().getPower() <= RankUnit.MODERATEUR.getPower()){
+            e.setQuitMessage(accouts.getRank().getPrefix() + player.getName() + " §fvien de quitter le serveur");
+        } else {
+            e.setQuitMessage(ChatColor.RED + "- " + ChatColor.WHITE + player.getName());
+        }
+    }
+
+    @EventHandler
+    public void onClick(InventoryClickEvent event) {
+
+        Inventory inv = event.getInventory();
+        Player player = (Player) event.getWhoClicked();
+        ItemStack current = event.getCurrentItem();
+
+        if (current == null) return;
+
+        if (inv.getName().equalsIgnoreCase("§7Sanctions pour ")) {
+
+        }
+    }
+
 }
 
 
